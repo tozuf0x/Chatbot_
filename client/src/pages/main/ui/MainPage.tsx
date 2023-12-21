6ec771320c3e5029d06696bca689abd4c2b1a5d7
Пронин Роман Андреевич <ra.pronin@severstal.com>
2023-12-21 13:28:30 +0300

    fix: исправляет ошибки в тексте
    * Исправляет заголовок главной страницы
    * Исправляет текст неверного ввода данных для поля кода ошибки

--- client/src/pages/main/ui/MainPage.tsx
+++ client/src/pages/main/ui/MainPage.tsx
@@ -20,9 +20,10 @@ export function MainPage() {
 
         <Content className={styles.content}>
           <Title className={styles.title} level={1}>
-            Интерфейс администрирования для ведения базы данных для чат-бота
+            Интерфейс администрирования для ведения базы данных чат-бота
             поддержки пользователей по&nbsp;ошибкам направления P2P&nbsp;S4
           </Title>
+
           <GuidancesTable />
           <ModalEntry />
         </Content>
